17d30e5a11257e22dd716a1f482aed57651f7f5b
Rodrigo Fernández <rodrigo@aquih.com>
2024-10-11 23:09:05 +0000

    Crear ajustes

--- models/stock.py
+++ models/stock.py
@@ -3,6 +3,9 @@
 from odoo import models, fields, api, Command, _
 from odoo.exceptions import UserError, ValidationError
 
+import requests
+import logging
+
 class Stock(models.Model):
     _name = 'advancloud.stock'
     _rec_name = 'code'
@@ -16,3 +19,39 @@ class Stock(models.Model):
     company_id = fields.Many2one('res.company', string='Company', required=True)
     warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse', required=True, check_company=True)
     location_id = fields.Many2one('stock.location', string='Location', required=True, check_company=True)
+
+    def create_inventories(self):
+        for w in self:
+            data = {
+                'grant_type': 'password',
+                'username': self.company_id.user_advancloud,
+                'password': self.company_id.password_advancloud,
+                'client_id': 'cloud'
+            }
+            r = requests.post('https://{}/advancloud/oauth/token'.format(self.company_id.url_advancloud), data=data)
+            logging.warning(r.text)
+
+            access_token = r.json()['access_token']
+
+            headers = { 'Authorization': 'Bearer '+access_token }
+            data = {
+                'username': self.company_id.user_advancloud,
+                'password': self.company_id.password_advancloud,
+                'token': self.company_id.app_advancloud,
+                'shop': self.warehouse_id.shop_id_advancloud,
+                'reporttype': 'json',
+            }
+            r = requests.post('https://{}/advancloud/import/stock/download/{}'.format(self.company_id.url_advancloud, self.code), data=data, headers=headers)
+            logging.warning(r.text)
+
+            result = r.json()
+
+            products = {}
+            for data in result['data']:
+                if data['productid'] not in products:
+                    products[data['productid']] = { 'product_id': data['productid'], 'location_id': self.location_id.id, 'inventory_quantity': 0}
+                products[data['productid']]['inventory_quantity'] += 1
+
+            self.env['stock.quant'].with_context(inventory_mode=True).create(products.values())
+
+        return self.env['stock.quant'].action_view_inventory()
